f552d0c66fee1fc51df40b1d62430dcd34a9a28b
MartinMore <maore_2332@hotmail.com>
2015-01-31 00:48:21 -0500

    Commint hecho a las 0.48

---
+++ src/com/classnested/ClassNestedStatic.java
@@ -0,0 +1,16 @@
+
+package com.classnested;
+
+public class ClassNestedStatic {
+    
+    
+    static class StaticNestedClass {
+        
+    }
+    
+    
+    class InnerClass {
+        
+    }
+    
+}
