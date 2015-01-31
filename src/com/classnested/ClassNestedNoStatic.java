518a0448d10a0c6b34277b6bad9e3dd5f0b043ec
MartinMore <maore_2332@hotmail.com>
2015-01-31 01:45:12 -0500

    Commint hecho a las 1.45

---
+++ src/com/classnested/ClassNestedNoStatic.java
@@ -0,0 +1,15 @@
+
+package com.classnested;
+
+public class ClassNestedNoStatic {
+    
+    // Clase Anidada NO ESTATICA
+    class InnerClass {
+        
+    }
+    
+    public static void main(String[] args) {
+        
+        
+    }
+}
--- src/com/classnested/ClassNestedStatic.java
+++ src/com/classnested/ClassNestedStatic.java
@@ -3,13 +3,23 @@ package com.classnested;
 
 public class ClassNestedStatic {
     
-    
+    // Clase Anidada Estatica
     static class StaticNestedClass {
         
     }
     
-    
+    /*
+        Clase Anidada NO ESTATICA o
+        inner class
+    */
     class InnerClass {
+        
+    }
+    
+    public static void main(String[] args) {
+        // Forma de accedes a una clase ANIDAD ESTATICA
+        ClassNestedStatic.StaticNestedClass
+                nestedObject = new ClassNestedStatic.StaticNestedClass();
         
     }
     
